c666da7ec0c1f792c03bfdb4f01af5bc6136ab4d
Sourav Das <sdunity12061999@gmail.com>
2021-10-31 10:01:26 +0530

    Added Posters in Landing page

---
+++ Data/index.data.js
@@ -0,0 +1,18 @@
+let sliderImages = [
+  "https://files.myglamm.com/site-images/original/Homepage-Desktop-DIwali.jpg",
+  "https://files.myglamm.com/site-images/original/Makeup-Homepage-Desktop.jpg",
+  "https://files.myglamm.com/site-images/original/Homepage-banner-desktop---1920-x-527-.jpg",
+  "https://files.myglamm.com/site-images/original/Homepagebanner-Web.jpg",
+  "https://files.myglamm.com/site-images/original/1920x527_2.jpg",
+];
+let posters = [
+  "https://files.myglamm.com/site-images/original/2240X614-BRIDAL.jpg",
+  "https://files.myglamm.com/site-images/original/Strip-banner-Web.jpg",
+  "https://files.myglamm.com/site-images/original/2-RANGE-Pouch-Offer-Sleek-Banner-Desktop_1.jpg",
+  "https://files.myglamm.com/site-images/original/SK555-desktop-sleek.jpg",
+  "https://files.myglamm.com/site-images/original/Virtual-Try-On_12.jpg",
+  "https://files.myglamm.com/site-images/original/banner@2x-3_2.jpg",
+  "https://files.myglamm.com/site-images/original/Community-Banner-2240-x-344_1.jpg",
+];
+
+export { sliderImages, posters };
