c5f9f16f9e926df7a60a55d9354ce62f0b442a04
Sarang Gunjal <72076630+Sarang0702@users.noreply.github.com>
2021-11-02 13:20:42 +0530

    add nav in productdetails page

---
+++ productnav/productnav.js
@@ -0,0 +1,61 @@
+function nav()
+{
+
+    return `
+
+    <div id="Main">
+        <div class="d1">
+            <img src="https://files.myglamm.com/site-images/original/MyGlamm-Logo_1.jpg" id="img1">
+        </div>
+        <div class="d2">
+            <input id="txt1" type="text" placeholder="Find Lipstick,Eyeliner,Makeup Tutorial etc.">
+        </div>
+        <div class="d3">
+            <button class="btn1">Refer & Earn</button>
+        </div>
+        <div class="d4">
+            <img src="https://cdn-icons-png.flaticon.com/512/126/126515.png" id="img2">
+        </div>
+        <div class="d5">
+            <img src="https://cdn-icons-png.flaticon.com/512/1077/1077063.png" id="img3">
+        </div>
+    </div>
+    <div id="Main1">
+        <div class="d1">
+            <h5 id="home">HOME</h5>
+        </div>
+        <div class="d6">
+            <h5
+            id="collection">COLLECTION</h5>
+        </div>
+        <div class="d2">
+            <h5 id="makeup">MAKEUP</h5>
+        </div>
+        <div class="d3">
+            <h5 id="haircare">HAIR CARE</h5>
+        </div>
+        <div class="d4">
+            <h5 id="skincare">SKIN CARE</h5>
+        </div>
+        <div class="d5">
+            <h5 id="sant">SANITIZING CARE</h5>
+        </div>
+        <div class="d7">
+            <h5 id="rewards">REWARDS</h5>
+        </div>
+        <div class="d8">
+            <h5 id="glam">MYGLAMM STUDIO</h5>
+        </div>
+        <div class="d9">
+            <h5 id="offers">OFFERS</h5>
+        </div>
+        </div>
+
+    `
+
+    
+
+}
+
+
+export default nav;
