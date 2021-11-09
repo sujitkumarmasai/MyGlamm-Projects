c5afdee7adfe8f77b62182444c577b82d13e613e
Sarang Gunjal <72076630+Sarang0702@users.noreply.github.com>
2021-11-09 09:31:19 +0530

    seperated js and css

---
+++ product/product.js
@@ -0,0 +1,427 @@
+import data from "../Data/products.data.js";
+import nav from "../productnav/productnav.js";
+
+var container = document.getElementById("container");
+container.innerHTML = nav() ;
+
+
+var pdata = document.getElementById("pdata");
+var cathead = document.getElementById("cathead");
+
+cathead.innerHTML = "  COLLECTION  ";
+
+    data.forEach( i => 
+    {
+        
+        let div = document.createElement("div");
+    
+        let img = document.createElement("img");
+        let title = document.createElement("p");
+        let subtitle = document.createElement("p");
+        let price = document.createElement("p");
+
+        let imgdiv = document.createElement("div");
+        
+        img.src = i.displayimg;
+        imgdiv.append(img);
+
+        title.innerText = i.title;
+        subtitle.innerText = i.subtitle;
+        subtitle.style.color = "rgb(147, 143, 143)";
+
+        price.innerHTML = "₹ "+i.price;
+
+        div.append(imgdiv,title,subtitle,price);
+
+        div.onmouseover = function()
+        {
+            div.style.border = "3px solid rgb(167, 165, 165)";
+            img.style.height = "170px";
+            img.style.width = "200px";
+            img.style.marginLeft = "10px";
+        }
+
+        div.onmouseleave = function()
+        {
+            div.style.border = null;
+            img.style.height = null;
+            img.style.width = null ;
+            img.style.marginLeft = null;
+        }
+
+        div.onclick = function()
+                {
+                    if(localStorage.getItem("pdetails")===null)
+                    {
+                        localStorage.setItem("pdetails",JSON.stringify([]));
+                    }
+                    
+                    let pdetails = JSON.parse(localStorage.getItem("pdetails"));
+                    
+                    pdetails.pop()
+                    pdetails.push(i);
+
+                    localStorage.setItem("pdetails",JSON.stringify (pdetails));
+                    
+                    window.location.href = "productdetails.html";
+
+                }
+
+        pdata.append(div);
+        
+    });
+
+    var makeup = document.getElementById("makeup");
+    makeup.onmouseover = function()
+    {
+        makeup.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    makeup.onmouseleave = function()
+    {
+        makeup.style.borderBottom = null;
+    }
+
+    makeup.onclick = function()
+    {
+        pdata.innerHTML = null;
+        cathead.innerHTML = "  MAKEUP  ";
+
+        data.forEach(i =>
+        {
+            if(i.category == "makeup")
+            {
+                
+                let div = document.createElement("div");
+                
+                let img = document.createElement("img");
+                let title = document.createElement("p");
+                let subtitle = document.createElement("p");
+                let price = document.createElement("p");
+
+                let imgdiv = document.createElement("div");
+                
+                img.src = i.displayimg;
+                imgdiv.append(img);
+
+                title.innerText = i.title;
+                subtitle.innerText = i.subtitle;
+                subtitle.style.color = "rgb(147, 143, 143)";
+
+                price.innerHTML = "₹ "+i.price;
+
+                div.append(imgdiv,title,subtitle,price);
+
+                div.onmouseover = function()
+                {
+                    div.style.border = "3px solid rgb(167, 165, 165)";
+                    img.style.height = "170px";
+                    img.style.width = "200px";
+                    img.style.marginLeft = "10px";
+                }
+
+                div.onmouseleave = function()
+                {
+                    div.style.border = null;
+                    img.style.height = null;
+                    img.style.width = null ;
+                    img.style.marginLeft = null;
+                }
+
+                div.onclick = function()
+                        {
+                            if(localStorage.getItem("pdetails")===null)
+                            {
+                                localStorage.setItem("pdetails",JSON.stringify([]));
+                            }
+                            
+                            let pdetails = JSON.parse(localStorage.getItem("pdetails"));
+                            
+                            pdetails.pop()
+                            pdetails.push(i);
+
+                            localStorage.setItem("pdetails",JSON.stringify (pdetails));
+                            
+                            window.location.href = "productdetails.html";
+
+                        }
+
+                pdata.append(div);
+            }
+        });
+    }
+
+    var haircare = document.getElementById("haircare");
+    haircare.onmouseover = function()
+    {
+        haircare.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    haircare.onmouseleave = function()
+    {
+        haircare.style.borderBottom = null;
+    }
+
+
+    haircare.onclick = function()
+    {
+        pdata.innerHTML = null;
+        cathead.innerHTML = "  HAIR CARE  ";
+
+        data.forEach(i =>
+        {
+            if(i.category == "haircare")
+            {
+                
+                let div = document.createElement("div");
+                
+                let img = document.createElement("img");
+                let title = document.createElement("p");
+                let subtitle = document.createElement("p");
+                let price = document.createElement("p");
+
+                let imgdiv = document.createElement("div");
+                
+                img.src = i.displayimg;
+                imgdiv.append(img);
+
+                title.innerText = i.title;
+                subtitle.innerText = i.subtitle;
+                subtitle.style.color = "rgb(147, 143, 143)";
+
+                price.innerHTML = "₹ "+i.price;
+
+                div.append(imgdiv,title,subtitle,price);
+
+                div.onmouseover = function()
+                {
+                    div.style.border = "3px solid rgb(167, 165, 165)";
+                    img.style.height = "170px";
+                    img.style.width = "200px";
+                    img.style.marginLeft = "10px";
+                }
+
+                div.onmouseleave = function()
+                {
+                    div.style.border = null;
+                    img.style.height = null;
+                    img.style.width = null ;
+                    img.style.marginLeft = null;
+                }
+
+                div.onclick = function()
+                        {
+                            if(localStorage.getItem("pdetails")===null)
+                            {
+                                localStorage.setItem("pdetails",JSON.stringify([]));
+                            }
+                            
+                            let pdetails = JSON.parse(localStorage.getItem("pdetails"));
+                            
+                            pdetails.pop()
+                            pdetails.push(i);
+
+                            localStorage.setItem("pdetails",JSON.stringify (pdetails));
+                            
+                            window.location.href = "productdetails.html";
+
+                        }
+
+                pdata.append(div);
+            }
+        });
+    }
+
+    var sanitizingcare = document.getElementById("sant");
+    sanitizingcare.onmouseover = function()
+    {
+        sanitizingcare.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    sanitizingcare.onmouseleave = function()
+    {
+        sanitizingcare.style.borderBottom = null;
+    }
+
+    sanitizingcare.onclick = function()
+    {
+        pdata.innerHTML = null;
+        cathead.innerHTML = "  SANITIZING CARE  ";
+
+        data.forEach(i =>
+        {
+            if(i.category == "sanitizingcare")
+            {
+                
+                let div = document.createElement("div");
+                
+                let img = document.createElement("img");
+                let title = document.createElement("p");
+                let subtitle = document.createElement("p");
+                let price = document.createElement("p");
+
+                let imgdiv = document.createElement("div");
+                
+                img.src = i.displayimg;
+                imgdiv.append(img);
+
+                title.innerText = i.title;
+                subtitle.innerText = i.subtitle;
+                subtitle.style.color = "rgb(147, 143, 143)";
+
+                price.innerHTML = "₹ "+i.price;
+
+                div.append(imgdiv,title,subtitle,price);
+
+                div.onmouseover = function()
+                {
+                    div.style.border = "3px solid rgb(167, 165, 165)";
+                    img.style.height = "170px";
+                    img.style.width = "200px";
+                    img.style.marginLeft = "10px";
+                }
+
+                div.onmouseleave = function()
+                {
+                    div.style.border = null;
+                    img.style.height = null;
+                    img.style.width = null ;
+                    img.style.marginLeft = null;
+                }
+
+                div.onclick = function()
+                        {
+                            if(localStorage.getItem("pdetails")===null)
+                            {
+                                localStorage.setItem("pdetails",JSON.stringify([]));
+                            }
+                            
+                            let pdetails = JSON.parse(localStorage.getItem("pdetails"));
+                            
+                            pdetails.pop()
+                            pdetails.push(i);
+
+                            localStorage.setItem("pdetails",JSON.stringify (pdetails));
+                            
+                            window.location.href = "productdetails.html";
+
+                        }
+
+                pdata.append(div);
+            }
+        });
+    }
+
+    var skincare = document.getElementById("skincare");
+    skincare.onmouseover = function()
+    {
+        skincare.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    skincare.onmouseleave = function()
+    {
+        skincare.style.borderBottom = null;
+    }
+
+
+    skincare.onclick = function()
+    {
+        pdata.innerHTML = null;
+        cathead.innerHTML = "  SKIN CARE  ";
+
+        data.forEach(i =>
+        {
+            if(i.category == "skincare")
+            {
+                
+                let div = document.createElement("div");
+                
+                let img = document.createElement("img");
+                let title = document.createElement("p");
+                let subtitle = document.createElement("p");
+                let price = document.createElement("p");
+
+                let imgdiv = document.createElement("div");
+                
+                img.src = i.displayimg;
+                imgdiv.append(img);
+
+                title.innerText = i.title;
+                subtitle.innerText = i.subtitle;
+                subtitle.style.color = "rgb(147, 143, 143)";
+
+                price.innerHTML = "₹ "+i.price;
+
+                div.append(imgdiv,title,subtitle,price);
+
+                div.onmouseover = function()
+                {
+                    div.style.border = "3px solid rgb(167, 165, 165)";
+                    img.style.height = "170px";
+                    img.style.width = "200px";
+                    img.style.marginLeft = "10px";
+                }
+
+                div.onmouseleave = function()
+                {
+                    div.style.border = null;
+                    img.style.height = null;
+                    img.style.width = null ;
+                    img.style.marginLeft = null;
+                }
+
+                div.onclick = function()
+                        {
+                            if(localStorage.getItem("pdetails")===null)
+                            {
+                                localStorage.setItem("pdetails",JSON.stringify([]));
+                            }
+                            
+                            let pdetails = JSON.parse(localStorage.getItem("pdetails"));
+                            
+                            pdetails.pop()
+                            pdetails.push(i);
+
+                            localStorage.setItem("pdetails",JSON.stringify (pdetails));
+                            
+                            window.location.href = "productdetails.html";
+
+                        }
+
+                pdata.append(div);
+            }
+        });
+    }
+
+    
+    var collection = document.getElementById("collection");
+    collection.onmouseover = function()
+    {
+        collection.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    collection.onmouseleave = function()
+    {
+        collection.style.borderBottom = null;
+    }
+
+    collection.onclick = function()
+    {
+        window.location.href = "products.html";
+    }
+
+    var home = document.getElementById("home");
+    home.onmouseover = function()
+    {
+        home.style.borderBottom = "3px solid rgb(255, 21, 138)"
+    }
+    home.onmouseleave = function()
+    {
+        home.style.borderBottom = null;
+    }
+
+    home.onclick = function()
+    {
+        window.location.href = "index.html";
+    }
+
+    var user = document.getElementById("user");
+    user.onclick = function()
+    {
+        window.location.href = "index.html";
+    }
---
+++ productdetails/productdetails.js
@@ -0,0 +1,119 @@
+import nav from "../productnav/productnav.js";
+
+var container = document.getElementById("container");
+container.innerHTML = nav() ;
+
+let items = JSON.parse(localStorage.getItem("pdetails"));
+let dict = items[0];
+console.log(dict);
+
+let img1 = document.getElementById("pimg1");
+let img2 = document.getElementById("pimg2");
+let img3 = document.getElementById("pimg3");
+let img4 = document.getElementById("pimg4");
+
+let img0 = document.getElementById("pimg0");
+
+let img01 = document.createElement("img");
+img01.src = dict.img1;
+img1.append(img01);
+
+let img02 = document.createElement("img");
+img02.src = dict.img2;
+img2.append(img02);
+
+let img04 = document.createElement("img");
+img04.src = dict.img4;
+img4.append(img04);
+
+let img03 = document.createElement("img");
+img03.src = dict.img3;
+img3.append(img03);
+
+let img05 = document.createElement("img");
+img05.src = dict.displayimg;
+img0.append(img05);
+
+img1.onmouseover = function()
+{
+    img0.innerHTML = null;
+	let img_0=document.createElement("img");
+	img_0.src=dict.img1;
+	img0.append(img_0);    
+}
+img1.onmouseleave = function()
+{
+    outimg();
+}
+
+img2.onmouseover = function()
+{
+    img0.innerHTML = null;
+	let img_0=document.createElement("img");
+	img_0.src=dict.img2;
+	img0.append(img_0);    
+}
+img2.onmouseleave = function()
+{
+    outimg();
+}
+
+img3.onmouseover = function()
+{
+    img0.innerHTML = null;
+	let img_0=document.createElement("img");
+	img_0.src=dict.img3;
+	img0.append(img_0);    
+}
+img3.onmouseleave = function()
+{
+    outimg();
+}
+
+img4.onmouseover = function()
+{
+    img0.innerHTML = null;
+	let img_0=document.createElement("img");
+	img_0.src=dict.img4;
+	img0.append(img_0);    
+}
+img4.onmouseleave = function()
+{
+    outimg();
+}
+
+function outimg()
+{
+        img0.innerHTML = null;
+		let img_0=document.createElement("img");
+		img_0.src=dict.displayimg;
+		img0.append(img_0);
+}
+
+let title = document.getElementById("title");
+let subtitle = document.getElementById("subtitle");
+let rating = document.getElementById("rating");
+let price = document.getElementById("price");
+
+title.innerHTML = dict.title;
+subtitle.innerHTML = dict.subtitle;
+rating.innerHTML = "Rating : "+dict.rating;
+price.innerHTML = "₹ "+dict.price;
+
+let pbagbutton = document.getElementById("pbagbutton");
+pbagbutton.onclick = function()
+{
+    if(localStorage.getItem("myglammCart")===null)
+    {
+        localStorage.setItem("myglammCart",JSON.stringify([]));
+    }
+    let myglammCart =JSON.parse(localStorage.getItem("myglammCart"));
+                    
+    myglammCart.push(dict);
+
+    localStorage.setItem("myglammCart",JSON.stringify (myglammCart));
+                    
+    // window.location.href = "./Cart/cart.html";
+    alert("Product added to cart");
+
+}
